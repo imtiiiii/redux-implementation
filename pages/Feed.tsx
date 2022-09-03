80fb0cb4b6d7c4ffc68671ed40319997439632bb
imtiiiii <imtiazahmed026@gmail.com>
2022-09-03 18:22:41 +0600

    update

--- pages/Feed.tsx
+++ pages/Feed.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import FeedLeft from './FeedLeft';
+
 import FeedMiddle from './FeedMiddle';
-import FeedRight from './FeedRight';
+
 
 function Feed() {
     return(
         <div className="_courseporium_social_layout_inner_wrap_area">
             <div className="_custom_row _dis_flex">
-               {/* <FeedLeft /> */}
+              
                <FeedMiddle />
-               {/* <FeedRight /> */}
+              
             </div>
         </div>
     )
